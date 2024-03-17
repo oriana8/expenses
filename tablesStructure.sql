5e1b7c92a89fb3a5fd79ee9d7b5969d670eeb413
Oriana Moreno <ori.moreno.sanchez@gmail.com>
2024-03-17 11:38:55 -0500

    coreccion estructura de tablas

--- tablesStructure.sql
+++ tablesStructure.sql
@@ -10,8 +10,11 @@ payment_status_name VARCHAR(15)
 );
 CREATE TABLE currencies(
 id_currency INT AUTO_INCREMENT PRIMARY KEY,
-currenciey_name VARCHAR(15)
+currency_name VARCHAR(15)
 );
+ALTER TABLE currencies CHANGE COLUMN id_currencies id_currency INT;
+ALTER TABLE currencies CHANGE COLUMN currencies_name currency_name VARCHAR(15);
+
 CREATE TABLE fixed_costs(
 id_fixed_cost INT AUTO_INCREMENT PRIMARY KEY,
 fixed_cost_name VARCHAR(50), 
@@ -22,16 +25,21 @@ payment_date DATE,
 autopay boolean,
 payment_status INT, 
 comments VARCHAR(200),
-FOREIGN KEY (currency) REFERENCES currencies(id_currencies)
+FOREIGN KEY (currency) REFERENCES currencies(id_currency),
+FOREIGN KEY (payment_method) REFERENCES payment_methods(id_payment_method),
+FOREIGN KEY (payment_status) REFERENCES payment_status(id_payment_status)
 );
 
 CREATE TABLE variable_costs(
 id_variable_cost INT AUTO_INCREMENT PRIMARY KEY,
 variable_cost_name VARCHAR(50), 
-currency VARCHAR(4),
-payment_method INT FOREIGN KEY,
+currency INT,
+payment_method INT,
 cost FLOAT,
 payment_date DATE,
-payment_status INT FOREIGN KEY, 
-comments VARCHAR(200)
+payment_status INT, 
+comments VARCHAR(200),
+FOREIGN KEY (payment_method) REFERENCES payment_methods(id_payment_method),
+FOREIGN KEY (payment_status) REFERENCES payment_status(id_payment_status),
+FOREIGN KEY (currency) REFERENCES currencies(id_currency)
 );
